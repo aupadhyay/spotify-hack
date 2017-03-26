f06ad6a6e88863524561534f00240e531e963c50
HsHack <hshack@e1z2r8p26.42.us.org>
2017-03-25 17:41:19 -0700

    added css

--- dashboard.php
+++ dashboard.php
@@ -78,8 +78,101 @@
 	<head></head>
 
 	<body>
-		<?php for($j=0; $j < count($playlists); $j++){ ?>
-			<a href="download.php?id=<?php echo $playlists[$j][1];?>&aid=<?php echo $final_access; ?>"><?php echo $playlists[$j][0]; ?></a>
-		<?php } ?>
+		
+			<a href=""></a>
+		
 	</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SPOTIFY WEB HACK - GONE WRONG</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<style>
+	@import url('https://fonts.googleapis.com/css?family=Montserrat:300,600,700|Muli:200,300,400');
+		body{
+			background-color: rgba(1,1,1,0.87);
+		}
+
+		.title{
+			color:white;
+			font-family: "Montserrat";
+			font-weight:600;
+			letter-spacing: 2.5px;
+			margin-top: 50px;
+			margin-bottom:30px;
+		}
+
+		.album-image{
+			margin-top: 20px;
+			transition: 0.3s;
+			width:150px;
+			height:150px;
+		}
+		.album-image:hover{
+			opacity: 0.3;
+		}
+
+		.container{
+			background-color: rgba(1,1,1,0.4);
+		}
+
+		.album-name{
+			font-family: "Muli";
+			font-weight:200;
+			color:white;
+			font-size: 20px;
+			padding:10px;
+			letter-spacing: 0.5px
+
+		}
+
+		.spotifyhr{
+			width:100px;
+			border-width:2.5px;
+			border-color:#1ed760;
+			margin-top: -10px;
+		}
+
+	</style>
+</head>
+<body>
+	<div class = "">
+		<center><img src = "http://www.chapelroswell.com/wp-content/uploads/2016/07/6274-spotify-logo-horizontal-white-rgb.png" style = "width:25%;margin-top:100px">
+		<h1 class = "title">CHOOSE YOUR PLAYLIST</h1></center>
+		<hr class = "spotifyhr">
+	</div>
+
+		<div class = "container">
+
+			<div class = "row">
+			<?php for($j=0; $j < count($playlists); $j++){ ?>
+				<div class = "col-md-2">
+					
+					<div class = "playlist-holder">
+						<center>
+							<a href = "download.php?id=<?php echo $playlists[$j][1];?>&aid=<?php echo $final_access; ?>" ><img src = "<?php echo $playlists[$j][2]; ?>" class = "album-image"></a>
+							<p class = "album-name"><?php echo $playlists[$j][0]; ?></p>
+
+						</center>
+
+					</div>
+					
+				</div>
+				<?php } ?>
+			</div>
+		</div>
+
+		
+	</div>
+</body>
 </html>
